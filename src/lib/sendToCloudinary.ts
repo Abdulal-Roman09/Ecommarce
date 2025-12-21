4d2deb28c507e9215a5252874d80aec080e721cc
Abdul Al Roman <mdroman45678910@gmail.com>
2025-12-21 22:42:14 +0600

    feat:sendToClounary add function

---
+++ src/lib/sendToCloudinary.ts
@@ -0,0 +1,18 @@
+import cloudinary from "./cloudinary"
+import fs from 'fs'
+
+const sendToCloudinary = async (file: any) => {
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload(file.path,
+            (error: Error, result: any) => {
+                fs.unlinkSync(file.path)
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(result)
+                }
+            }
+        )
+    })
+}
+export default sendToCloudinary
